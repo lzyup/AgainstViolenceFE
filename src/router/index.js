403926fee482166a0d6aac9b9856acd7d01c27e1
Luzy <felixxg@foxmail.com>
2019-10-30 18:17:08 +0800

    合并代码

--- src/router/index.js
+++ src/router/index.js
@@ -46,6 +46,13 @@ export const constantRoutes = [
   {
     path: '/',
     component: Layout,
+    redirect: '/dashboard',
+    children: [{
+      path: 'dashboard',
+      name: 'Dashboard',
+      component: () => import('@/views/dashboard/index'),
+      meta: { title: 'Dashboard', icon: 'dashboard' }
+    }]
   },
   {
     path: '/hc', //helping center
